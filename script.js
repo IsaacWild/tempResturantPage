16a79a9d86d2dc8832f0169bd24d170290f74755
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2022-05-16 10:55:06 +0100

    code swing is not processing JS into webpage preview

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const pageLoad = (() => {
+function pageLoad (){
     const pageContent = document.querySelector(".container");
     const header = document.createElement('div')
     header.classList.add('header')
     header.textContent = 'test'
     pageContent.appendChild(header)
-})();
+};
+
+pageLoad();
